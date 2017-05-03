7900a9318419d3a2838320125f9e12d450dc9077
leo-epi <leonardo.epifanio@hotmail.com>
2017-05-04 00:52:33 +0100

    comidas

--- js/foods.js
+++ js/foods.js
@@ -6,6 +6,7 @@ var FOOD_ITEMS = {
             _img: "images/foods/bolocaco.jpg",
             _classif: 4,
             _ctime: 2,
+            _ingredientsString: "Pão e manteiga de alho.",
             _ingredients: ["Manteiga de Alho"] },
 
         { _name: "Camarão",
@@ -14,6 +15,7 @@ var FOOD_ITEMS = {
             _img: "images/foods/camaroes.jpg",
             _classif: 5,
             _ctime: 1,
+            _ingredientsString: "Camarão e molho cocktail.",
             _ingredients: ["Molho cocktail"] },
 
         { _name: "Salada de Polvo",
@@ -22,6 +24,7 @@ var FOOD_ITEMS = {
             _img: "images/foods/saladapolvo.jpg",
             _classif: 5,
             _ctime: 1,
+            _ingredientsString: "Polvo, cebola, alho e pimentos.",
             _ingredients: ["Cebola", "Alho", "Pimentos"] },
     ],
     "menu-sopas": [
@@ -31,6 +34,7 @@ var FOOD_ITEMS = {
             _img: "images/foods/sagriao.jpg",
             _classif: 4,
             _ctime: 3,
+            _ingredientsString: "Agrião, cenoura e batata",
             _ingredients: ["Agrião", "Cenoura"] },
 
         { _name: "Caldo Verde",
@@ -39,6 +43,7 @@ var FOOD_ITEMS = {
             _img: "images/foods/caldoverde.jpg",
             _classif: 5,
             _ctime: 3,
+            _ingredientsString: "Couve, choriço, alho e batata.",
             _ingredients: [ "Choriço", "Alho"] },
 
         { _name: "Sopa de Castanha",
@@ -47,6 +52,7 @@ var FOOD_ITEMS = {
             _img: "images/foods/scastanha.jpg",
             _classif: 4,
             _ctime: 3,
+            _ingredientsString: "Castanha, alho e batata.",
             _ingredients: ["Cebola", "Alho"] },
     ],
     "menu-pratos": [
@@ -56,6 +62,7 @@ var FOOD_ITEMS = {
             _img: "images/foods/bitoque.jpg",
             _classif: 5,
             _ctime: 5,
+            _ingredientsString: "Carne de porco, arroz, salada, ovo e batatas fritas.",
             _ingredients: ["Arroz", "Salada", "Ovo", "Batatas fritas"] },
 
         { _name: "Espetada",
@@ -64,6 +71,7 @@ var FOOD_ITEMS = {
             _img: "images/foods/espetada.jpg",
             _classif: 4,
             _ctime: 10,
+            _ingredientsString: "Carne de vaca, arroz, salada e milho frito.",
             _ingredients: ["Arroz", "Salada", "Milho frito"] },
 
         { _name: "Picado de Carne",
@@ -72,15 +80,17 @@ var FOOD_ITEMS = {
             _img: "images/foods/picado.jpg",
             _classif: 5,
             _ctime: 8,
+            _ingredientsString: "Carne de vaca, batatas fritas, arroz, salada e molho de natas.",
             _ingredients: ["Batata", "Arroz", "Salada", "Molho de natas"] },
     ],
     "menu-bebidas": [
         { _name: "Cerveja Coral",
             _price: 125,
-            _desc: "Gasolina da Madeira.",
+            _desc: "Cerveja preferida do famoso Cristiano Ronaldo.",
             _img: "images/foods/cerveja.jpg",
             _classif: 3,
             _ctime: 1,
+            _ingredientsString: "Cerveja.",
             _ingredients: [] },
 
         { _name: "Coca-Cola",
@@ -89,6 +99,7 @@ var FOOD_ITEMS = {
             _img: "images/foods/cocacola.jpg",
             _classif: 4,
             _ctime: 1,
+            _ingredientsString: "Coca-cola.",
             _ingredients: [] },
 
         { _name: "Brisa-Maçã",
@@ -97,6 +108,7 @@ var FOOD_ITEMS = {
             _img: "images/foods/brisa.jpg",
             _classif: 5,
             _ctime: 8,
+            _ingredientsString: "Brisa-Maçã.",
             _ingredients: [] },
     ],
     "menu-sobremesas": [
@@ -106,6 +118,7 @@ var FOOD_ITEMS = {
             _img: "images/foods/broas.jpg",
             _classif: 5,
             _ctime: 1,
+            _ingredientsString: "Mel.",
             _ingredients: [] },
 
         { _name: "Arroz Doce",
@@ -114,6 +127,7 @@ var FOOD_ITEMS = {
             _img: "images/foods/arrozdoce.jpg",
             _classif: 4,
             _ctime: 1,
+            _ingredientsString: "Arroz, leite e canela.",
             _ingredients: [] },
 
         { _name: "Serradura",
@@ -122,6 +136,7 @@ var FOOD_ITEMS = {
             _img: "images/foods/serradura.jpg",
             _classif: 4,
             _ctime: 1,
+            _ingredientsString: "Bolacha, natas e leite.",
             _ingredients: [] },
     ],
     "menu-cafetaria": [
@@ -131,6 +146,7 @@ var FOOD_ITEMS = {
             _img: "images/foods/carioca.jpg",
             _classif: 3,
             _ctime: 1,
+            _ingredientsString: "Limão.",
             _ingredients: [] },
 
         { _name: "Café",
@@ -139,6 +155,7 @@ var FOOD_ITEMS = {
             _img: "images/foods/cafe.jpg",
             _classif: 4,
             _ctime: 1,
+            _ingredientsString: "Café.",
             _ingredients: [] },
 
         { _name: "Galão",
@@ -147,6 +164,7 @@ var FOOD_ITEMS = {
             _img: "images/foods/galao.jpg",
             _classif: 5,
             _ctime: 1,
+            _ingredientsString: "Café e leite.",
             _ingredients: [] },
     ]
 };
