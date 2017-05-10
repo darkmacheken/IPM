452d65f4d0c02db848eb4a40ec29d605c38201be
leo-epi <leonardo.epifanio@hotmail.com>
2017-05-10 21:30:36 +0100

    musicas

---
+++ js/music.js
@@ -0,0 +1,43 @@
+var MUSICS = {
+        { _name: "I'm Blue",
+            _artist: "Eiffel 65",
+            _playtime: 221,
+            _cover: "images/musica/blue.jpg",
+            _votes: 0 },
+
+        { _name: "Summer Of 69",
+            _artist: "Bryan Adams",
+            _playtime: 221,
+            _cover: "images/musica/bryan-adams.jpg",
+            _votes: 0 },
+
+        { _name: "Johnny B. Goode",
+            _artist: "Chuck Berry",
+            _playtime: 161,
+            _cover: "images/musica/chuck-berry.jpg",
+            _votes: 0 },
+
+        { _name: "Take On Me",
+            _artist: "a-ha",
+            _playtime: 227,
+            _cover: "images/musica/a-ha.jpg",
+            _votes: 0 },
+
+        { _name: "Down Under",
+            _artist: "Men At Work",
+            _playtime: 221,
+            _cover: "images/musica/men-at-work.jpg",
+            _votes: 0 },
+
+        { _name: "Roundabout",
+            _artist: "Yes",
+            _playtime: 516,
+            _cover: "images/musica/yes.jpg",
+            _votes: 0 },
+
+        { _name: "Ocean Man",
+            _artist: "Ween",
+            _playtime: 127,
+            _cover: "images/musica/ween.jpg",
+            _votes: 0 },
+};
