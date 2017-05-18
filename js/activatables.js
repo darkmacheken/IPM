e532ca2af13182f7e4756fec97c94f618fb9e233
pedrodaniel10 <pedro.daniel.l@tecnico.ulisboa.pt>
2017-05-18 14:49:05 +0100

    Activables

--- js/activatables.js
+++ js/activatables.js
@@ -1,7 +1,11 @@
 function prepareActivatables() {
     $(document).mouseup(function () {
-        $(".opcao").removeClass("js_active");
+        $(".opcao,.opcao3ecran,.confirmarbtn,.submit").removeClass("js_active");
     });
+    activatables_opcao3ecran();
+     activatables_confirmar();
+     activatables_confirmar2()
+     activatables_submit();
 }
 
 function activatables_opcao() {
@@ -10,6 +14,46 @@ function activatables_opcao() {
     });
 
     $(".info").mousedown(function (e) {
+        e.stopPropagation();
+    });
+}
+function activatables_opcao3ecran() {
+    $(".opcao3ecran").mousedown(function (e) {
+        $(this).addClass("js_active");
+    });
+
+    $(".info3ecran").mousedown(function (e) {
+        e.stopPropagation();
+    });
+}
+
+function activatables_confirmar() {
+    $(".confirmarbtn").mousedown(function (e) {
+        $(this).addClass("js_active");
+    });
+
+    $(".disabler").mousedown(function (e) {
+        e.stopPropagation();
+    });
+}
+
+
+function activatables_confirmar2() {
+    $(".confirmOk2btn").mousedown(function (e) {
+        $(this).addClass("js_active");
+    });
+
+    $(".disabler").mousedown(function (e) {
+        e.stopPropagation();
+    });
+}
+
+function activatables_submit() {
+    $(".submit").mousedown(function (e) {
+        $(this).addClass("js_active");
+    });
+
+    $(".disabler").mousedown(function (e) {
         e.stopPropagation();
     });
 }
